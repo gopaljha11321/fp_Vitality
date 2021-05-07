ab38da8b857b4526256f2ec853147d0a1807f4d2
Gopal Jha <82890881+gopaljha11321@users.noreply.github.com>
2021-05-08 00:15:26 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,42 @@
+console.clear();
+var a=document.getElementById('access');
+var inn=a.innerHTML;
+var objpeople =[
+    {
+        username: "gopal",
+        password: "8745977703"
+    },
+    {
+        username:"utkarsh",
+        password:"12345678"
+
+    },
+    {
+        username:"arjun",
+        password:"87654321",
+    },
+]
+function getInfo(){
+    var username=document.getElementById("username").value;
+    var password=document.getElementById("password").value;
+
+ for (i=0;i< objpeople.length;i++)
+ {
+if(username==objpeople[i].username && password==objpeople[i].password  )
+{
+    console.log("dcndjcmdc")
+    inn= "Welcome"+" "+username+"!!";
+    a.innerHTML=inn;
+    window.location="https://gopaljha11321.github.io/css-1/"
+    return;
+}
+ 
+else
+{
+    console.log("username and password is wrong");
+    inn= "username and password is wrong";
+    a.innerHTML=inn;
+}
+ }
+}
+
