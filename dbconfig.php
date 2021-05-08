33da2405d5e0ba15fce7d6ce0b385784ede1655b
Gopal Jha <82890881+gopaljha11321@users.noreply.github.com>
2021-05-08 16:29:46 +0530

    Add files via upload

---
+++ dbconfig.php
@@ -0,0 +1,6 @@
+<?php 
+define('DB_USER', "id16761678_vitality0");     // Your database user name
+define('DB_PASSWORD', "Mait@00714807320");			// Your database password (mention your db password here)
+define('DB_DATABASE', "id16761678_vitality"); // Your database name
+define('DB_SERVER', "localhost");			// db server (Mostly will be 'local' host)
+?>
