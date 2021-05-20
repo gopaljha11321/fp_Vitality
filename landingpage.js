774070fe6eb4c2a9ec5ba3a31248f5dfa7eb0a4d
Gopal Jha <82890881+gopaljha11321@users.noreply.github.com>
2021-05-20 11:09:03 +0530

    Update landingpage.js

--- landingpage.js
+++ landingpage.js
@@ -7,8 +7,15 @@
                     var val= data;
                   var humid=(data['weather'][(Object.keys(data['weather']).length)-1]['hum']);
                    var temper=(data['weather'][(Object.keys(data['weather']).length)-1]['temp']);
+		   var body=(data['weather'][(Object.keys(data['weather']).length)-1]['body']);
+		   var heart= (data['weather'][(Object.keys(data['weather']).length)-1]['heart']);
+		   var oxygen = (data['weather'][(Object.keys(data['weather']).length)-1]['oxygen']);
                    document.getElementById("temperature").innerHTML = temper;
                    document.getElementById("humidity").innerHTML = humid+"%";
+		   document.getElementById("body_tmp").innerHTML = body;
+		   document.getElementById("heart_rate").innerHTML = heart;
+		   document.getElementById("oxygen").innerHTML = oxygen;
+					
 					console.log(data['weather'][(Object.keys(data['weather']).length)-1]['temp']);
 				});
             
